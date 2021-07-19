8ba6d5ea44ee784c5d8d8fb676637b62fcd5cf23
isakvlarsson <isaklar@kth.se>
2021-07-20 01:20:13 +0200

    Add audio processing

--- src/plugin.rs
+++ src/plugin.rs
@@ -25,6 +25,25 @@ impl Plugin for SyntWave {
             ..Default::default()
         }
     }
+
+    fn process(&mut self, buffer: &mut vst::buffer::AudioBuffer<f32>) {
+        // `buffer.split()` gives us a tuple containing the 
+        // input and output buffers.  We only care about the
+        // output, so we can ignore the input by using `_`.
+        let (_, output_buffer) = buffer.split();
+        
+        // Now, we want to loop over our output channels.  This
+        // includes our left and right channels (or more, if you
+        // are working with surround sound).
+        for output_channel in output_buffer.into_iter() {
+            // Let's iterate over every sample in our channel.
+            for output_sample in output_channel {
+                // For every sample, we want to add a random value from
+                // -1.0 to 1.0.
+                *output_sample = 0f32;
+            }
+        }
+    }
 }
 
 plugin_main!(SyntWave); 
