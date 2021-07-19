a06d2c5c23dc770a398f57cbb65f9e1df3ddb8ac
Morris Hansing <hansing@kth.se>
2021-07-20 01:19:37 +0200

    'test' case

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 use super::*;
+use super::wave::*;
+use super::synth::*;
 
 use rodio::{OutputStream, source::Source};
 
@@ -9,9 +11,16 @@ fn main() {
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    let source = Wave::square(440.0,0.025);
+    let w1 = Wave::square(440.0,0.025);
+    let w2 = Wave::saw(80.0,0.025);
+    let w3 = Wave::sine(180.0,0.025);
 
-    stream_handle.play_raw(source.convert_samples());
+    let mut synth = WaveSynth::new();
+    synth.push_addative_wave(w1);
+    synth.push_addative_wave(w2);
+    synth.push_subtractive_wave(w3);
 
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    stream_handle.play_raw(synth);
+
+    std::thread::sleep(std::time::Duration::from_secs(3));
 }
